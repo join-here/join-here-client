a53b11fbbdff9e4584e8aeb15b64697b5860f4a6
tunggary <gsy1656@gmail.com>
2023-02-10 22:38:03 +0900

    Refactor 이미지 최적화 기능 추가

--- hooks/useForm.js
+++ hooks/useForm.js
@@ -1,9 +1,10 @@
+import { lowResolution } from "@utils/image";
 import { useCallback, useState } from "react";
 
 export function useForm(initialValue, setFunction) {
   const [value, setValue] = useState(initialValue);
   const onChange = useCallback(
-    (e) => {
+    async (e) => {
       let newValue = e.target.value;
 
       if (setFunction) {
@@ -12,7 +13,7 @@ export function useForm(initialValue, setFunction) {
 
       if (e.target.type === "file") {
         if (!e.target.files || e.target.files.length === 0) return;
-        newValue = e.target.files[0];
+        newValue = await lowResolution(e.target.files[0]);
       }
 
       setValue((prev) => ({
