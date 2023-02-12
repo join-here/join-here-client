ffe95fb0ffe959b7b7f2c5d655bb87ade369778e
tunggary <gsy1656@gmail.com>
2023-02-12 23:53:15 +0900

    Refactor home 어두운 footer 적용

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
 import PageWrapper from "@components/common/PageWrapper";
 import Banner from "@components/home/Banner";
 import Category from "@components/home/Category";
+import useFooterColor from "@hooks/useFooterColor";
 
 export default function Home() {
+  const { setFooterColor } = useFooterColor();
+
+  useEffect(() => {
+    setFooterColor("dark");
+    return () => setFooterColor("white");
+  }, []);
+
   return (
     <PageWrapper theme="white">
       <Banner />
